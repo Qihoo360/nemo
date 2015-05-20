460b07a1252c19fa8564a2a29c5108266b65aeb5
KernelMaker <songzhao.asm@icloud.com>
2015-05-20 10:55:14 +0800

    first blood by sz

--- include/nemo.h
+++ include/nemo.h
@@ -1,21 +1,33 @@
 #ifndef NEMO_INCLUDE_NEMO_H_
 #define NEMO_INCLUDE_NEMO_H_
 
-#include <stdio.h>
-#include <string>
+//#include <stdio.h>
+//#include <string>
 
-#include "rocksdb/status.h"
-#include "rocksdb/options.h"
+//#include "rocksdb/status.h"
+//#include "rocksdb/options.h"
 #include "rocksdb/db.h"
+#include "nemo_iterator.h"
 
 namespace nemo {
-using namespace rocksdb;
+//using namespace rocksdb;
+typedef rocksdb::Options Options;
+typedef rocksdb::Status Status;
+typedef rocksdb::Slice Slice;
+
+//class nemo::KIterator;
 
 class Nemo
 {
 public:
     Nemo(const std::string &db_path, Options options);
     ~Nemo();
+//    Iterator *iterator(const string &start, const string &end, uint64_t limit);
+//    Iterator *rev_iterator(const string &start, const string &end, uint64_t limit);
+
+    Status Set(const std::string &key, const std::string &val);
+    Status Get(const std::string &key, std::string *val);
+    KIterator* scan(const Slice &start, const Slice &end, uint64_t limit);
 
     /*
      * The Hash function interface
@@ -24,10 +36,11 @@ public:
     Status HGet(const std::string &key, const std::string &field, std::string *val);
     Status HKeys(const std::string &key, std::vector<std::string> *fields);
 
+
 private:
 
     std::string db_path_;
-    std::unique_ptr<DB> db_;
+    std::unique_ptr<rocksdb::DB> db_;
 
     Nemo(const Nemo &rval);
     void operator =(const Nemo &rval);
---
+++ src/nemo_kv.cc
@@ -0,0 +1,40 @@
+#include "nemo_kv.h"
+#include "iterator.h"
+#include "xdebug.h"
+
+rocksdb::Status nemo::Nemo::Set(const std::string &key, const std::string &val) {
+    rocksdb::Status s;
+    std::string buf = encode_kv_key(key);
+    s = db_->Put(rocksdb::WriteOptions(), buf, val);
+    log_info("Set return  %s", s.ToString().c_str());
+    return s;
+}
+
+rocksdb::Status nemo::Nemo::Get(const std::string &key, std::string *val) {
+    rocksdb::Status s;
+    std::string buf = encode_kv_key(key);
+    s = db_->Get(rocksdb::ReadOptions(), buf, val);
+    log_info("Get return %s", s.ToString().c_str());
+    return s;
+}
+
+nemo::KIterator* nemo::Nemo::scan(const rocksdb::Slice &start, const rocksdb::Slice &end, uint64_t limit) {
+    std::string key_start, key_end;
+    key_start = encode_kv_key(start);
+    if(end.empty()){
+        key_end = "";
+    }else{
+        key_end = encode_kv_key(end);
+    }
+    //dump(key_start.data(), key_start.size(), "scan.start");
+    //dump(key_end.data(), key_end.size(), "scan.end");
+    rocksdb::Iterator *it;
+    rocksdb::ReadOptions iterate_options;
+    iterate_options.fill_cache = false;
+    it = db_->NewIterator(iterate_options);
+    it->Seek(start.ToString());
+    if(it->Valid() && it->key() == start){
+        it->Next();
+    }
+    return new KIterator(new Iterator(it, end.ToString(), limit)); 
+}
