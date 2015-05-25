6a436baf1fa3a49e58b8ad8501020c55bfd11938
songzhao <songzhao@360.cn>
2015-05-25 18:02:20 +0800

    double kill

--- src/nemo.cc
+++ src/nemo.cc
@@ -5,51 +5,21 @@
 
 namespace nemo {
 
-using namespace rocksdb;
+//using namespace nemo;
 
-Nemo::Nemo(const std::string &db_path, Options options) :
+Nemo::Nemo(const std::string &db_path, rocksdb::Options options) :
     db_path_(db_path)
 {
-    DB* db;
-    Status s = DB::Open(options, db_path_, &db);
+    pthread_mutex_init(&(writer_kv_.writer_mutex), NULL);
+    rocksdb::DB* db;
+    rocksdb::Status s = rocksdb::DB::Open(options, db_path_, &db);
     if (!s.ok()) {
         log_err("open db %s error %s", db_path_.c_str(), s.ToString().c_str());
     } else {
         log_info("open db success");
     }
     
-    db_ = std::unique_ptr<DB>(db);
+    db_ = std::unique_ptr<rocksdb::DB>(db);
 }
-
-Status Nemo::HSet(const std::string &key, const std::string &field, const std::string &val)
-{
-    Status s;
-    s = db_->Put(rocksdb::WriteOptions(), key + field, val);
-    log_info("val %s", val.c_str());
-    return s;
-}
-
-Status Nemo::HGet(const std::string &key, const std::string &field, std::string *val)
-{
-    Status s;
-
-    s = db_->Get(rocksdb::ReadOptions(), key + field, val);
-    log_info("val %s", val->c_str());
-
-    return s;
-}
-
-Status Nemo::HKeys(const std::string &key, std::vector<std::string> *fields)
-{
-    Status s;
-    rocksdb::Iterator* it = db_->NewIterator(rocksdb::ReadOptions());
-    for (it->Seek(key); it->Valid() && it->key().ToString() < key + '$'; it->Next()) {
-        log_info("key %s val %s", it->key().ToString().c_str(), it->value().ToString().c_str());
-    }
-    assert(it->status().ok());
-    delete it;
-    return s;
-}
-
 };
 
