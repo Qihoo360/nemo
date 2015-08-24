a1a298212b78596b595e7706128703b7c479b4d8
songzhao <songzhao.asm@icloud.com>
2015-08-24 18:59:39 +0800

    change the last parameter of zrangebyscore from limit to offset

--- include/nemo_iterator.h
+++ include/nemo_iterator.h
@@ -73,7 +73,7 @@ public:
     ZIterator(Iterator *it, const rocksdb::Slice &key);
     ~ZIterator();
     bool Valid() { return it_->Valid(); };
-    bool Skip(uint64_t offset) { return it_->Skip(offset); };
+    bool Skip(int64_t offset) { if (offset < 0) {return true;} else {return it_->Skip(offset);} };
     bool Next();
     std::string Key() { return key_; };
     double Score() { return score_; };
--- src/nemo_zset.cc
+++ src/nemo_zset.cc
@@ -180,8 +180,9 @@ Status Nemo::ZRange(const std::string &key, const int64_t start, const int64_t s
     }
 }
 
-Status Nemo::ZRangebyscore(const std::string &key, const double mn, const double mx, std::vector<SM> &sms, uint64_t limit) {
-    ZIterator *iter = ZScan(key, mn, mx, limit);
+Status Nemo::ZRangebyscore(const std::string &key, const double mn, const double mx, std::vector<SM> &sms, int64_t offset) {
+    ZIterator *iter = ZScan(key, mn, mx, -1);
+    iter->Skip(offset);
     while(iter->Next()) {
         sms.push_back({iter->Score(), iter->Member()});
     }
