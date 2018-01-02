5e82c6b5468b5d66aee5bde5bce6cac0aff2b692
leviathan1995 <leviathan0992@gmail.com>
2018-01-02 16:09:36 +0800

    support multiple compression algorithm

--- include/nemo_options.h
+++ include/nemo_options.h
@@ -4,6 +4,11 @@
 namespace nemo {
 
 struct Options {
+  enum CompressionType {
+    kNoCompression,
+    kSnappyCompression,
+    kZlibCompression
+  };
     bool create_if_missing;
     int write_buffer_size;
     int max_open_files;
@@ -13,7 +18,7 @@ struct Options {
     // default target_file_size_base and multiplier is the save as rocksdb
     int target_file_size_base;
     int target_file_size_multiplier;
-    bool compression;
+    CompressionType compression;
     int max_background_flushes;
     int max_background_compactions;
     int max_bytes_for_level_multiplier;
@@ -25,7 +30,7 @@ struct Options {
         write_threads(71),
         target_file_size_base(64 * 1024 * 1024),
         target_file_size_multiplier(1),
-        compression(true),
+        compression(kSnappyCompression),
         max_background_flushes(1),
         max_background_compactions(1),
         max_bytes_for_level_multiplier(10) {}
